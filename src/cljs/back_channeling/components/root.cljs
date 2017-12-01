401529b5414a1317c58650a648101facbb8e5ec6
miyamoen <akihito.biblioteca@gmail.com>
2017-12-01 14:29:51 +0900

    reorder

--- src/cljs/back_channeling/components/root.cljs
+++ src/cljs/back_channeling/components/root.cljs
@@ -111,20 +111,18 @@
           ;                                                  :message (str (:user/name user) " is calling!!")}))}
           ;         (om/build avatar member)])]])]
           [:div.ui.dropdown.item
-           [:div {:on-click (fn [_]
+           [:div.ui.two.column.grid.text.center {:on-click (fn [_]
                               (om/set-state! owner :open-profile? (not open-profile?)))}
-            (om/build avatar user)
+
             [:span
-             [:i.icon.circle.large
-              {:class (case (:socket app) :connect "teal" :disconnect "red")}]
-             (:user/name user)]]
-           [:div.menu.transition {:class (if open-profile? "visible" "hidden")}
-            [:div.item {:on-click (fn [_]
+             [:i.icon.circle
+              {:class (case (:socket app) :connect "teal" :disconnect "red")
+              :on-click (fn [_]
                                     (when (= (:socket app) :disconnect)
-                                      (put! (om/get-shared owner :msgbox) [:reconnect-socket])))}
-             [:i.icon.circle {:class (case (:socket app) :connect "teal" :disconnect "red")}]
-             (case (:socket app) :connect "connecting" :disconnect "reconnect")]
-            [:div.divider]
+                                      (put! (om/get-shared owner :msgbox) [:reconnect-socket])))}]
+             (:user/name user)]
+             (om/build avatar user)]
+           [:div.menu.transition {:class (if open-profile? "visible" "hidden")}
             [:form.item {:action (str (om/get-shared owner :prefix) "/logout")
                          :method :post
                          :name "logout"
