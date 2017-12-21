d773a2ce8c9b7300018e45af4285f4c7c0d2ee63
miyamoen <akihito.biblioteca@gmail.com>
2017-12-21 16:53:26 +0900

    Guard no reference errpr

--- src/cljs/back_channeling/notification.cljs
+++ src/cljs/back_channeling/notification.cljs
@@ -1,19 +1,19 @@
 (ns back-channeling.notification
   (:use [back-channeling.components.avatar :only [avatar-url]]))
 
-(def initialized? (atom (and js/Notification
-                             (= (.. js/Notification -permission) "granted"))))
+(def initialized? (atom (and (.-Notification js/window)
+                             (= (.. (.-Notification js/window) -permission) "granted"))))
 
 (defn initialize []
   (when (and (not @initialized?)
-             js/Notification)
+             (.-Notification js/window))
     (.requestPermission js/Notification)
     (reset! initialized? true)))
 
 (defn show [message]
-  (when js/Notification
+  (when (.-Notification js/window)
     (let [notification (js/Notification.
-                        (str "From " (get-in message [:comment/posted-by :user/name]) " @BackChanneling") 
+                        (str "From " (get-in message [:comment/posted-by :user/name]) " @BackChanneling")
                         (clj->js {:iconUrl (avatar-url (:comment/posted-by message))
                                   :icon (avatar-url (:comment/posted-by message))
                                   :body (:comment/content message)}))]
